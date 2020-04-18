66cbeee3be30169630a60694097a28c7728a3d57
Ji-hoon, Lee <zx1056@naver.com>
2020-04-18 23:40:25 +0900

    class EPNode

--- EightPuzzle/Program.cs
+++ EightPuzzle/Program.cs
@@ -1,16 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EightPuzzle
+﻿namespace EightPuzzle
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] initial  = new int[,] { { 2, 8, 3 }, { 1, 6, 4 }, { 7, 0, 5 } };
+            int[,] goal     = new int[,] { { 1, 2, 3 }, { 8, 0, 4 }, { 7, 6, 5 } };
+
+            new EPNode(initial, 2, 1, 0, null).Estimate(goal).Print()
+                .MoveUp().Estimate(goal).Print()
+                .MoveUp().Estimate(goal).Print()
+                .MoveLeft().Estimate(goal).Print()
+                .MoveDown().Estimate(goal).Print()
+                .MoveRight().Estimate(goal).Print();
+
+
+
+
+
+
+
+            //if ( new Puzzle(initial, goal).Solve() )
+            //{
+            //    Console.WriteLine("Solved!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Failed!");
+            //}
         }
     }
 }
